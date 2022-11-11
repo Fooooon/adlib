2819cccfcd29ae71866904aa3896b2c1975d9522
Fooooon <117962149+Fooooon@users.noreply.github.com>
2022-11-10 19:47:05 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+adj2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Trading options is absolutely {adj}! It makes me so excited because I love to {verb1}! Everyone in Fight Club\
+ is an absolute {adj2} even better than {famous_person} himself"
+
+print(madlib)
+
+
